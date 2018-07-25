2b418870a89a2a78fbcc108287595b1ea45af749
lei.sunstar <lei.suntar@gmail.com>
2018-07-26 00:30:04 +0800

    add qrcode

--- mini_program.go
+++ mini_program.go
@@ -4,27 +4,36 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-
 	"github.com/parnurzeal/gorequest"
+	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 type MiniProgramInterface interface {
 	GetSession(code string) (*Session, error)
 	Decode(encryptedData, iv string, v interface{}) error
+	GetWXacodeunLimit(scene, page string, width int, isHyaline bool, filePath string) (string, error)
 }
 
 type MiniProgramImpl struct {
-	AppId     string
-	AppSecret string
+	mu          sync.Mutex
+	AccessToken string
+	Expires     int64
+	AppId       string
+	AppSecret   string
 }
 
 func NewMiniProgramImpl() *MiniProgramImpl {
 	return &MiniProgramImpl{}
 }
+
 func (m *MiniProgramImpl) AddAppId(appId string) *MiniProgramImpl {
 	m.AppId = appId
 	return m
 }
+
 func (m *MiniProgramImpl) AddAppSecret(secret string) *MiniProgramImpl {
 	m.AppSecret = secret
 	return m
@@ -52,4 +61,53 @@ func (m *MiniProgramImpl) Decode(encryptedData, iv string, session *Session, v i
 		return err
 	}
 	return nil
+}
+
+func (m *MiniProgramImpl) GetWXacodeunLimit(scene, page string, width int, isHyaline bool, filePath string) (string, error) {
+	token, err := m.getAccessToken()
+	if err != nil {
+		return "", err
+	}
+	s := &QrCode{
+		Scene:     scene,
+		Page:      page,
+		Width:     width,
+		IsHyaline: isHyaline,
+	}
+	var errs []error
+	_, body, errs := gorequest.New().Post(fmt.Sprintf(GetWxacodeunLimitUrl, token)).
+		Retry(3, 5*time.Second, http.StatusBadRequest, http.StatusInternalServerError).
+		SendStruct(s).
+		End()
+	if errs != nil {
+		return "", errors.New(fmt.Sprintf("%v", errs))
+	}
+	if len(filePath) > 0 {
+		f, err := os.Create(filePath)
+		if err != nil {
+			return "", err
+		}
+		defer f.Close()
+		f.WriteString(body)
+	}
+	return body, nil
+}
+
+func (m *MiniProgramImpl) getAccessToken() (string, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if len(m.AccessToken) > 0 {
+		if time.Now().Unix() < m.Expires {
+			return m.AccessToken, nil
+		}
+	}
+	ac := &AccessToken{}
+	_, _, errs := gorequest.New().Get(fmt.Sprintf(GetAccessTokenUrl,
+		m.AppId, m.AppSecret)).EndStruct(ac)
+	if errs != nil {
+		return "", errors.New(fmt.Sprintf("%v", errs))
+	}
+	m.AccessToken = ac.Token
+	m.Expires = time.Now().Unix() + int64(ac.ExpiresIn)
+	return m.AccessToken, nil
 }
--- type.go
+++ type.go
@@ -1,8 +1,9 @@
 package miniprogram
 
 const (
-	JsCode2SessionUrl = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
-	GetAccessTokenUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
+	JsCode2SessionUrl    = "https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code"
+	GetAccessTokenUrl    = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
+	GetWxacodeunLimitUrl = "https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=%s"
 )
 
 type User struct {
@@ -28,4 +29,21 @@ type Session struct {
 	SessionKey string `json:"session_key"`
 	ExpiresIn  int    `json:"expires_in"`
 	UnionId    string `json:"unionid"`
+}
+
+type AccessToken struct {
+	Token     string `json:"access_token"`
+	ExpiresIn int    `json:"expires_in"`
+}
+
+type QrCode struct {
+	Scene     string `json:"scene"`
+	Page      string `json:"page"`
+	Width     int    `json:"width"`
+	IsHyaline bool   `json:"is_hyaline"`
+}
+
+type QrCodeArgs struct {
+	QrCode
+	FilePath string `json:"file_path"`
 }
